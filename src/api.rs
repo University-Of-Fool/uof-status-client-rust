59a7ab1b566a86488825f3ac8b3bece5e4b65aee
UrsusFeline <ursusfeline07@gmail.com>
2023-03-24 10:05:56 +0000

    .

--- src/api.rs
+++ src/api.rs
@@ -1,16 +1,27 @@
 use reqwest::header::HeaderMap;
-use serde_json::value::Value;
-use std::collections::HashMap;
+use serde_json::{value::Value, json};
+use std::{collections::HashMap, fmt::format};
 
-pub async fn get(url: &str) -> Result<HashMap<String, String>, reqwest::Error> {
-    Ok(reqwest::get(url)
+//获取服务器列表
+pub async fn get_list(url: &str) -> Result<HashMap<String, String>, reqwest::Error> {
+    let urls = format!("{}/api/server/get", url);
+    Ok(reqwest::get(urls)
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?)
+}
+
+//获取状态
+pub async fn get_status(url: &str, id: u64) -> Result<HashMap<String, String>, reqwest::Error> {
+    let urls = format!("{}/api/status/get/{}", url, id);
+    Ok(reqwest::get(urls)
         .await?
         .json::<HashMap<String, String>>()
         .await?)
 }
 
 //创建服务器
-pub async fn new_server(
+pub async fn put_server(
     url: &str,
     token: &str,
     name: &str,
@@ -18,17 +29,67 @@ pub async fn new_server(
 ) -> Result<HashMap<String, Value>, reqwest::Error> {
     let client = reqwest::Client::new();
     let urls: String = format!("{}/api/server/put", url);
-    let tokens: String = format!("\"{}\"", token);
-    let names: String = format!("\"{}\"", name);
-    let descriptions: String = format!("\"{}\"", description);
-
     let mut headers = HeaderMap::new();
     headers.insert("Content-Type", "application/json".parse().unwrap());
 
     let mut data = HashMap::new();
-    data.insert("token", tokens);
-    data.insert("name", names);
-    data.insert("description", descriptions);
+    data.insert("token", token);
+    data.insert("name", name);
+    data.insert("description", description);
+
+    Ok(client
+        .post(urls)
+        .headers(headers)
+        .json(&data)
+        .send()
+        .await?
+        .json::<HashMap<String, Value>>()
+        .await?)
+}
+
+//删除服务器
+pub async fn drop_server(
+    url: &str,
+    token: &str,
+    id: u64
+) -> Result<HashMap<String, Value>, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let urls: String = format!("{}/api/server/drop", url);
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", "application/json".parse().unwrap());
+
+    let data = json!({
+        "token": token,
+        "serverId": id
+    });
+
+    Ok(client
+        .post(urls)
+        .headers(headers)
+        .json(&data)
+        .send()
+        .await?
+        .json::<HashMap<String, Value>>()
+        .await?)
+}
+
+//上传状态
+pub async fn put_status(
+    url: &str,
+    token: &str,
+    id: u64,
+    online: bool
+) -> Result<HashMap<String, Value>, reqwest::Error> {
+    let client = reqwest::Client::new();
+    let urls: String = format!("{}/api/status/put", url);
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", "application/json".parse().unwrap());
+
+    let data = json!({
+        "token": token,
+        "serverId": id,
+        "online": online
+    });
 
     Ok(client
         .post(urls)
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 use std::env;
+use log::{info, warn, error};
 mod api;
-use api::get;
-use api::new_server;
+use api::get_list;
+use api::get_status;
+use api::put_server;
+use api::drop_server;
+use api::put_status;
+extern crate log;
 
 #[tokio::main]
-async fn main() {
+fn main() {
+    env_logger::init();
+    let args: Vec<String> = env::args().collect();
+    match args[1] {
+        list => get_list(&args[2]),
+        status => get_status(&args[2], id),
+
+    };
+
+
+
+
+
+
+
     /*     if let Ok(res) = new_server(
         "http://localhost:4044",
         "aaabbbaaa",
@@ -15,7 +34,8 @@ async fn main() {
     {
         println!("{:#?}", res);
     }*/
-    let res = new_server("http://127.0.0.1", "aaabbbaaa", "Test1", "A test server").await;
+    let res = drop_server("http://117.50.173.86:4044", "aaabbbaaa", 1).await;
 
     println!("{:#?}", res);
+    warn!("test");
 }
