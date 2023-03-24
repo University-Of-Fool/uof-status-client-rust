b3abe9ca68a6223df82939090c9bad8e1053e2e1
UrsusFeline <ursusfeline07@gmail.com>
2023-03-25 00:03:04 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,22 @@
+use async_std::task;
+use log::{error, info, warn};
 use std::env;
-use log::{info, warn, error};
+use std::time::Duration;
 mod api;
-use api::get_list;
-use api::get_status;
-use api::put_server;
-use api::drop_server;
-use api::put_status;
+use api::{drop_server, get_list, get_status, put_server, put_status};
 extern crate log;
 
 #[tokio::main]
-fn main() {
+async fn main() {
+    env::set_var("RUST_LOG", "info");
     env_logger::init();
-    let args: Vec<String> = env::args().collect();
-    match args[1] {
-        list => get_list(&args[2]),
-        status => get_status(&args[2], id),
-
-    };
-
-
-
-
-
-
-
-    /*     if let Ok(res) = new_server(
-        "http://localhost:4044",
-        "aaabbbaaa",
-        "test",
-        "A test server",
-    )
-    .await
-    {
-        println!("{:#?}", res);
-    }*/
-    let res = drop_server("http://117.50.173.86:4044", "aaabbbaaa", 1).await;
-
-    println!("{:#?}", res);
-    warn!("test");
+    loop {
+        let res = put_status("http://127.0.0.1", "an1l7e3yk84p9bsc", 5, true).await;
+        info!("Uploading status.");
+        match res {
+            Ok(v) => info!("Succeed: {v:?}"),
+            Err(e) => error!("Fail: {e:?}"),
+        }
+        task::sleep(Duration::from_secs(5)).await;
+    }
 }
