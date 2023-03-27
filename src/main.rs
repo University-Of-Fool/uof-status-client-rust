3eff83f649a395190eb851227b2fe274732a3bd3
UrsusFeline <ursusfeline07@gmail.com>
2023-03-28 00:31:53 +0800

    .

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,183 @@
 use async_std::task;
+use clap::{arg, builder::Str, command, Args, Parser, Subcommand};
 use log::{error, info, warn};
+use std::env::args;
+use std::path::{Path, PathBuf};
 use std::time::Duration;
 use std::{env, str::FromStr};
 mod api;
-mod arguments;
-mod config;
 use api::{drop_server, get_list, get_status, put_server, put_status};
-use arguments::argument;
 extern crate clap;
 extern crate log;
 
-#[tokio::main]
+#[derive(Parser)]
+#[command(name = "uof-status")]
+#[command(author = "UrsusFeline <ursusfeline07@gmail.com>")]
+#[command(version = "0.1.0")]
+#[command(about = "A client of uof-status written in rust", long_about = None)]
+struct Cli {
+    #[arg(
+        short = 'c',
+        long = "config",
+        value_name = "FILE",
+        help = "Run with a configuration file"
+    )]
+    config: Option<PathBuf>,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Add a server to the primary server
+    put(Put),
+    /// Delete a server from the primary server
+    drop(Drop),
+    /// Start uploading status
+    status(Status),
+    /// Get a list of servers
+    list(List),
+    /// Query the server status
+    inquire(Inquire),
+}
+#[derive(Args)]
+struct Put {
+    #[arg(
+        short = 'u',
+        long = "url",
+        value_name = "URL",
+        help = "Input a API URL of main server"
+    )]
+    url: String,
+    #[arg(
+        short = 't',
+        long = "token",
+        value_name = "TOKEN",
+        help = "Input a global_token"
+    )]
+    token: String,
+    #[arg(
+        short = 'n',
+        long = "name",
+        value_name = "NAME",
+        help = "Input a server name"
+    )]
+    name: String,
+    #[arg(
+        short = 'd',
+        long = "description",
+        value_name = "REMARK",
+        help = "Input a description"
+    )]
+    description: String,
+}
+#[derive(Args)]
+struct Drop {
+    #[arg(
+        short = 'u',
+        long = "url",
+        value_name = "URL",
+        help = "Input a API URL of main server"
+    )]
+    url: String,
+    #[arg(
+        short = 't',
+        long = "token",
+        value_name = "TOKEN",
+        help = "Input a global_token"
+    )]
+    token: String,
+    #[arg(
+        short = 'i',
+        long = "id",
+        value_name = "ID",
+        help = "Input a server ID"
+    )]
+    id: u64,
+}
+#[derive(Args)]
+struct Status {
+    #[arg(
+        short = 'u',
+        long = "url",
+        value_name = "URL",
+        help = "Input a API URL of main server"
+    )]
+    url: String,
+    #[arg(
+        short = 't',
+        long = "token",
+        value_name = "TOKEN",
+        help = "Input a user token"
+    )]
+    token: String,
+    #[arg(
+        short = 'i',
+        long = "id",
+        value_name = "ID",
+        help = "Input a server ID"
+    )]
+    id: u64,
+    #[arg(
+        short = 'o',
+        long = "offline",
+        value_name = "STATUS",
+        help = "Upload offline status"
+    )]
+    offline: bool,
+}
+#[derive(Args)]
+struct List {
+    #[arg(
+        short = 'u',
+        long = "url",
+        value_name = "URL",
+        help = "Input a API URL of main server"
+    )]
+    url: String,
+}
+#[derive(Args)]
+struct Inquire {
+    #[arg(
+        short = 'u',
+        long = "url",
+        value_name = "URL",
+        help = "Input a API URL of main server"
+    )]
+    url: String,
+    #[arg(
+        short = 'i',
+        long = "id",
+        value_name = "ID",
+        help = "Input a server ID"
+    )]
+    id: u64,
+}
+
 async fn main() {
     env::set_var("RUST_LOG", "info");
     env_logger::init();
+    let cli = Cli::parse().await;
 
-    let url = "http://127.0.0.1";
-    let token = "an1l7e3yk84p9bsc";
-    let id = 5;
-    let online = true;
-    loop {
-        let res = put_status(url, token, id, online).await;
-        match res {
-            Ok(v) => info!("Uploading status to {},Succeed: {v:?}", url),
-            Err(e) => error!("Uploading statusto {},Fail: {e:?}", url),
+    match &cli.command {
+        Commands::status(Status) => {
+            let Status {
+                url,
+                token,
+                id,
+                offline,
+            } = Status;
+            let online = !offline;
+            loop {
+                let res = put_status(&url, &token, id, online).await;
+                match res {
+                    Ok(v) => info!("Uploading status to {},Succeed: {v:?}", url),
+                    Err(e) => error!("Uploading statusto {},Fail: {e:?}", url),
+                }
+                task::sleep(Duration::from_secs(5)).await;
+            }
         }
-        task::sleep(Duration::from_secs(5)).await;
+        Some(_) => todo!(),
     }
 }
