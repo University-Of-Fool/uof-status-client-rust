4454d94aa5e93e1ccb5e4f112ded7b0e7baca620
UrsusFeline <ursusfeline07@gmail.com>
2023-04-21 23:19:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-# Build script for batch cross-compilation
+## Build script for batch cross-compilation
 
-# Set build target
+## Set build target
+#TARGET=()
 TARGET=("x86_64-unknown-linux-gnu --no-default-features" "x86_64-unknown-linux-musl --no-default-features" "x86_64-pc-windows-gnu --no-default-features" "x86_64-pc-windows-msvc --no-default-features" "aarch64-unknown-linux-gnu --no-default-features" "aarch64-unknown-linux-musl --no-default-features" "aarch64-pc-windows-gnu --no-default-features" "aarch64-pc-windows-msvc --no-default-features")
 OUTPUT_FILE=(ustatc ustatc.exe)
 
-# Check environment
+## Check environment
 source "$HOME/.cargo/env"
 hash cargo || exit 1 && echo "NOTICE:Please make sure \"cargo\" is installed!"
 cargo install cross
 
-# Start to build
+## Start to build
 mkdir output
 for BUILD_TARGET in ${TARGET[@]}; do
 	cross build --release --target=$BUILD_TARGET
@@ -20,7 +21,7 @@ for BUILD_TARGET in ${TARGET[@]}; do
 	done
 done
 
-# Extra build from arguments
+## Extra build from arguments
 for BUILD_TARGET_ in $*; do
 	cross build --release --target=$BUILD_TARGET_
 	mkdir output/$BUILD_TARGET_
